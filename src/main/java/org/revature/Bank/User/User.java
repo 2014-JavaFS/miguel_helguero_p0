697a972dd739d1cd5f7cdf21eacf1dc17a86dd5e
Miguel <miguelihelguero@gmail.com>
2024-07-09 20:37:43 -0500

    integrated exception handling if invalid input was entered for email and password

--- src/main/java/org/revature/Bank/User/User.java
+++ src/main/java/org/revature/Bank/User/User.java
@@ -4,4 +4,38 @@ public class User {
     private String email;
     private String password;
     private double balance;
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+
+    public void setBalance(double balance) {
+        this.balance = balance;
+    }
+
+
+
+    public User(){}
+
+    public User(String email, String password){
+        this.email=email;
+        this.password=password;
+    }
 }
+

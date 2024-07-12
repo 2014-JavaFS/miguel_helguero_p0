9e11ec19c862b38136ae5f9fe3a5cc04bd1867ad
mhelguer <miguelihelguero@gmail.com>
2024-07-12 00:21:03 -0500

    fixed bank.sql to only include statements about the User table initialization

--- src/main/resources/bank.sql
+++ src/main/resources/bank.sql
@@ -3,7 +3,7 @@ CREATE TABLE users(
 	id serial primary key,
 	email VARCHAR(254) not null unique,
 	password VARCHAR(64) not NULL,
-	balance NUMERIC(26, 2) not null check(balance >= 0.00)
+	balance NUMERIC(26, 2) default 0.00 check(balance >= 0.00)
 );
 
 insert into users(email, password, balance)
@@ -14,31 +14,6 @@ values('miguel@email.com', 'miguelpassword', 20.24),
 ('zoey@email.com', 'zoeypassword', 121232.21),
 ('bob@email.com', 'bobpassword', 00.01);
 
-update users set balance = 00.00
-where email like '%email.c__%';
 select * from users;
 
-create type member_enum as enum('ADMIN', 'PILOT', 'PASSENGER');
-
-create table members(
-	member_id serial primary key,
-	first_name varchar(20),
-	last_name varchar(40),
-	email varchar(50),
-	member_type member_enum default 'PASSENGER',
-	password varchar(64)
-);
-
--- establish relationship btwn flights and members(pilots)
-alter table flights
-add constraint fk_pilot_member_id foreign key(pilot) 
-references members(member_id);
-
-
--- MEMBER STATEMENTS BELOW
-
-insert into members
-values(default, 'tommy', 'hoang', 'tommy@mail.com', default, 'pass123');
-
--- database admin adds pilot and an admin
 
